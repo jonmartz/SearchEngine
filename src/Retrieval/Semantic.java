5d5ddd450f18d195ee288f0cbb680d7b6641b279
adielazari <adiel010@gmail.com>
2018-12-23 09:52:06 +0200

    vector Distance

--- src/Retrieval/Semantic.java
+++ src/Retrieval/Semantic.java
@@ -57,8 +57,15 @@ public class Semantic {
     }
 
     private double getVectorDistance(ArrayList<Double> vector1,ArrayList<Double> vector2){
-
-
+        double dotProduct = 0.0;
+        double norm1 = 0.0;
+        double norm2 = 0.0;
+        for (int i = 0; i < vector1.size(); i++) {
+            dotProduct += vector1.get(i) * vector2.get(i);
+            norm1 += Math.pow(vector1.get(i), 2);
+            norm2 += Math.pow(vector2.get(i), 2);
+        }
+        return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
     }
 
 
